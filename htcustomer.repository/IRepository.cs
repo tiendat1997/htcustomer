b62c9427e9c692564e5ee742246810e976de45c1
tiendat <tiendatdeveloper1997@gmail.com>
2018-05-16 20:34:09 +0700

    [Week 2] Add EF, Create Repository

---
+++ htcustomer.repository/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace htcustomer.repository
+{
+    interface IRepository<T> where T : class
+    {
+        IEnumerable<T> Gets();
+        T GetByID(int id);
+        void Insert(T model);
+        void Delete(int id);
+        void Edit(T model);
+
+        void Save();
+
+    }
+}
---
+++ htcustomer.repository/Repository.cs
@@ -0,0 +1,55 @@
+﻿using htcustomer.entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+
+namespace htcustomer.repository
+{
+    class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly HuyThongDiaryDBEntities _context;
+        private readonly IDbSet<T> dbEntity;
+
+        public Repository()
+        {
+            _context = new HuyThongDiaryDBEntities();
+            dbEntity = _context.Set<T>();
+        }
+
+        public void Delete(int id)
+        {
+            T model = dbEntity.Find(id);
+            dbEntity.Remove(model);
+        }
+
+        public T GetByID(int id)
+        {
+            return dbEntity.Find(id);
+        }
+
+        public IEnumerable<T> Gets()
+        {
+            return dbEntity.ToList();
+        }
+
+        public void Insert(T model)
+        {
+            dbEntity.Add(model);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Edit(T model)
+        {
+            _context.Entry<T>(model).State = EntityState.Modified;
+        }
+    }
+}
