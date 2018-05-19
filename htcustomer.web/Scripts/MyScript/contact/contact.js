f06ec3e5a319c1ea43b4c8d0db12ef62d51cd743
tiendat <tiendatdeveloper1997@gmail.com>
2018-05-19 21:25:10 +0700

    [Week 2] Implement Loading during search customer

--- htcustomer.web/Scripts/MyScript/contact/contact.js
+++ htcustomer.web/Scripts/MyScript/contact/contact.js
@@ -1,6 +1,19 @@
-﻿$(function (e) {
+﻿// Showing Loading Icon when searching customer : 
+// isShowed = true -> show else hide
+function toggleLoadingCustomerIcon(isShowed) {
+    if (isShowed) {
+        $("#loading-customer").css("display", "block");
+        $(".address-book").css("opacity","0.3");
+    }
+    else {
+        $("#loading-customer").css("display", "none");
+        $(".address-book").css("opactiy","1");
+    }    
+}
+$(function (e) {
     $("#customer-search-form").on("submit", function (e) {
         e.preventDefault();
+        toggleLoadingCustomerIcon(true); // Show Loading Gif 
         var submitBtn = $(this).find("button[type=submit]");
         var url = $(submitBtn).data("url");
         var data = $(this).serialize();
@@ -11,7 +24,13 @@
             method: 'GET',
             cached: false
         }).done(function (result) {
-            $("#customer-search-result .row").replaceWith(result);
+            if (result != null) {
+                $("#customer-search-result .row").replaceWith(result);                
+            }
+            else {
+                alert("Error Occurs");
+            }
+            toggleLoadingCustomerIcon(false);            
         }).fail(function (e) {
             alert("Error occurs");
         });
